081861e11f1d4db478a9e0c1790c63055d0f32dc
Grejo Joby <grejo00@gmail.com>
2020-09-23 16:29:18 +0530

    Website

---
+++ precache-manifest.00c3332451a1e03665cc57eaf5139492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e3f286d2e1baae0bd035e8a1e0f799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "590087409f899255f77c",
+    "url": "/static/css/main.e9db6f73.chunk.css"
+  },
+  {
+    "revision": "eb5996828794eca462f6",
+    "url": "/static/js/2.32b8667c.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.32b8667c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590087409f899255f77c",
+    "url": "/static/js/main.b31efabc.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
